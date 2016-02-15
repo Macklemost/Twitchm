cc8828dd537a83aa4a3916ea48e9ceb656231b53
Macklemost <cmclemore@cole-mclemores-macbook-pro.local>
2016-02-14 23:13:58 -0600

    Initial Commit

---
+++ Twitchm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitchm
+//
+//  Created by Cole McLemore on 2/14/16.
+//  Copyright © 2016 Cole McLemore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
